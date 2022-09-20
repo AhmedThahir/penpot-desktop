2cab6fcd91fc52f51f9c4a314280c064f2a5ea93
Vijaye Raji <hello@korbsstudio.com>
2022-09-20 04:32:11 -0400

    Add files for Linux

---
+++ extra/linux/penpot-desktop.sh
@@ -0,0 +1,3 @@
+# This should never be a fixed name, since the version number in the file name will change when an update occurs.
+# By using "*", well it's a simple workaround. 
+./penpot-desktop-*
