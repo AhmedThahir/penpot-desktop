0f2059081be004f691056b686008724fc6ca2e7a
Vijaye Raji <hello@korbsstudio.com>
2022-09-23 21:07:25 -0400

    Add tabs - `electron-tabs` by Brrd

---
+++ src/base/js/view/tabs.js
@@ -0,0 +1,30 @@
+setTimeout(() => {
+  const tabGroup = document.querySelector("tab-group");
+    
+  // New Tab
+  tabGroup.setDefaultTab({
+    active: true,
+    src: "https://design.korbsstudio.com/",
+    webviewAttributes: {
+      allowpopups: true,
+    }
+  });
+
+  // Default Tab (Home)
+  const tab = tabGroup.addTab({
+    active: true,
+    src: "https://design.korbsstudio.com/",
+    webviewAttributes: {
+      allowpopups: true,
+    },
+    closable: false
+  });
+  tab.webview.addEventListener('close', () => t.close())
+
+  // Change Titlebar Automatically
+  const webview = tab.webview;
+  webview.addEventListener('page-title-updated', () => {
+    const newTitle = webview.getTitle();
+    tab.setTitle(newTitle);
+  });
+}, 1000)
