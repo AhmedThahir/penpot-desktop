1593f62fafe7c8f9142f67ad0378ebc287add80e
Korbs <ks@korbsstudio.com>
2022-09-24 09:02:37 -0400

    Remove Glasstron

--- src/process/index.js
+++ src/process/index.js
@@ -1,5 +1,4 @@
 const {app, BrowserWindow, dialog, ipcMain, ipcRenderer, Menu, MenuItem} = require('electron')
-const glasstron = require('glasstron')
 const {autoUpdater} = require("electron-updater");
 const log = require('electron-log');
 const path = require('path');
@@ -7,34 +6,30 @@ autoUpdater.logger = log;
 
 if (process.platform == 'darwin') {
   app.whenReady().then(() => {
-    mainWindow.webContents.executeJavaScript('isMac()')
     global.frame = false;
     global.titleBarStyle = 'hiddenInset';
-    global.blurType = 'vibrancy';
 })}
 else if(process.platform == 'win32'){
   app.whenReady().then(() => {
     global.frame = false;
     global.titleBarStyle = 'hidden';
-    global.blurType = 'acrylic'; // Acrylic won't be use here as there is a pully issue: https://github.com/KorbsStudio/glasstron-quick-start#there-is-mouse-latency-on-windows
 })}
 else{
   app.whenReady().then(() => {
     global.frame = true;
-    global.blurType = 'blurbehind';
+    global.titleBarStyle = 'default';
 })}
 
 const launch = () => {
-  const mainWindow = new glasstron.BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 1300,
     height: 900,
     minWidth: 1240,
     minHeight: 400,
+    show: false,
     autoHideMenuBar: true,
     darkTheme: true,
     frame: global.frame,
-    blur: true,
-    blurType: global.blurType,
     titleBarStyle: global.titleBarStyle,
     trafficLightPosition: { x: 10, y: 10 },
     titleBarOverlay: {
@@ -52,18 +47,9 @@ const launch = () => {
       nodeIntegration: false
     }
   })
-
-  if (process.platform == 'darwin') {app.whenReady().then(() => {mainWindow.webContents.executeJavaScript('isMac()')})} // Don't maximize window: #13 https://github.com/KorbsStudio/Penpot-Desktop/issues/13#issuecomment-1253246535
-  else if(process.platform == 'win32'){app.whenReady().then(() => {mainWindow.webContents.executeJavaScript('isWindows()'); setTimeout(() => {mainWindow.maximize()}, 2850);})}
-  else {setTimeout(() => {
-    mainWindow.maximize();
-  }, 2850)}
-
+  mainWindow.maximize();
+  mainWindow.show();
   mainWindow.loadFile('src/index.html')
-  
-  // While the blur looks nice on the splash, it's mostly hidden by the WebView until I can add transparency at some point (was possible in Electron v18 and broke in v19 and up).
-  // So to reduce CPU usage, let's disable the blur using Glasstron after about 5 seconds.
-  setTimeout(() => {mainWindow.setBlur(false)}, 5000);
 
   autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {mainWindow.webContents.executeJavaScript(`showUpdateAvailable()`)})
   ipcMain.on('updateApp',  () => {autoUpdater.quitAndInstall()})
