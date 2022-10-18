82e4998d460877faf91f5b0780dd2265571b5f66
Korbs <ks@korbsstudio.com>
2022-10-18 19:16:49 -0400

    Update 'extra/linux/install.sh'

--- extra/linux/install.sh
+++ extra/linux/install.sh
@@ -10,7 +10,7 @@
   done
     echo "$appEntry" | sudo -S tee ${launcher}
 
-# Final - Install Penpot Desktop
+# Install Penpot Desktop
     (
         echo "10" ;
         echo "# Creating Directory /usr/share/korbs-studio/" ;
@@ -23,18 +23,18 @@
         echo "20" ;
         echo "# Downloading and adding resources..."
         cd /usr/share/korbs-studio/penpot-desktop/
-        sudo wget -q "https://raw.githubusercontent.com/KorbsStudio/Penpot-Desktop/main/build/icon.png"
+        sudo wget -q "https://code.korbsstudio.com/KorbsStudio/Penpot-Desktop/raw/branch/main/build/icon.png"
 
         echo "40" ;
         cd /usr/share/korbs-studio/penpot-desktop/app/
-        sudo wget -q "https://updates.korbsstudio.com/penpot-desktop/penpot-desktop-0.0.91.AppImage"
+        sudo wget -q "https://updates.korbsstudio.com/penpot-desktop/Penpot%20Desktop-0.1.1.AppImage"
         echo "80" ;
-        sudo wget -q "https://raw.githubusercontent.com/KorbsStudio/Penpot-Desktop/main/extra/linux/penpot-desktop.sh"
+        sudo wget -q "https://code.korbsstudio.com/KorbsStudio/Penpot-Desktop/raw/branch/main/extra/linux/penpot-desktop.sh"
 
         echo "95" ;
         echo "# Almost done..."
         cd /usr/share/applications/
-        sudo wget -q "https://raw.githubusercontent.com/KorbsStudio/Penpot-Desktop/main/extra/linux/penpot-desktop.desktop"
+        sudo wget -q "https://code.korbsstudio.com/KorbsStudio/Penpot-Desktop/raw/branch/main/extra/linux/penpot-desktop.desktop"
         sudo chmod -R 755 -R /usr/share/korbs-studio/penpot-desktop/
 
         # This is add 'penpot-desktop' as a command, because why not?
@@ -52,8 +52,8 @@ zenity --info --ellipsize --title="Job complete" --text="Finished installing."
 sudo -K
 clear
 echo "If Penpot Desktop installed successfully, it should appear in your applications menu soon. Thanks for installing and happy designing!"
-echo "Was there an issue? Submit it here: https://github.com/KorbsStudio/Penpot-Desktop/issues or you can email 'support@korbsstudio.com'"
-echo "Wanna contribute? Create a PR: https://github.com/KorbsStudio/Penpot-Desktop"
+echo "Was there an issue? Submit it here: https://code.korbsstudio.com/KorbsStudio/Penpot-Desktop/issues or you can email 'support@korbsstudio.com'"
+echo "Wanna contribute? Create a PR: https://code.korbsstudio.com/KorbsStudio/Penpot-Desktop"
 
 # Run this as the very last command
 exec bash
