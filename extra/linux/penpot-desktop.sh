30c8ce39b04b4c57ea7eb40110943854fdcba379
Korbs <ks@korbsstudio.com>
2022-10-18 19:26:35 -0400

    Update 'extra/linux/penpot-desktop.sh'

--- extra/linux/penpot-desktop.sh
+++ extra/linux/penpot-desktop.sh
@@ -1,3 +1,3 @@
 # This should never be a fixed name, since the version number in the file name will change when an update occurs.
 # By using "*", well it's a simple workaround. 
-/usr/share/korbs-studio/penpot-desktop/app/penpot-desktop-*.AppImage
+/usr/share/korbs-studio/penpot-desktop/app/Penpot-*.AppImage
