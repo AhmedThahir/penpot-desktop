5734731bcdafe14ad92cea98e816721cf9c8fa6d
Korbs <ks@korbsstudio.com>
2022-11-21 13:11:48 -0500

    update

---
+++ src/base/js/view/onboarding.js
@@ -0,0 +1,11 @@
+function onboardingSlide1() {document.querySelector('.onboarding #one').style.display = 'flex'}
+function onboardingSlide2() {document.querySelector('.onboarding #two').style.display = 'flex'}
+function onboardingSlide3() {document.querySelector('.onboarding #three').style.display = 'flex'}
+function onboardingSlide4() {document.querySelector('.onboarding #four').style.display = 'flex'}
+
+function hideAllSlides() {
+    document.querySelector('.onboarding #one').style.display = 'none'
+    document.querySelector('.onboarding #two').style.display = 'none'
+    document.querySelector('.onboarding #three').style.display = 'none'
+    document.querySelector('.onboarding #four').style.display = 'none'
+}
