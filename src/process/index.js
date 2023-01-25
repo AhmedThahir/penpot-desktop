73d06766778e31be10ef7fda746b23b3a1a6cfe8
KorbsStudio <ks@korbsstudio.com>
2023-01-25 10:52:04 -0500

    update

--- src/process/index.js
+++ src/process/index.js
@@ -1,9 +1,6 @@
-const {app, BrowserWindow, ipcMain, ipcRenderer, Menu, MenuItem, powerMonitor} = require('electron')
-
-// Use native titlebar from the OS on Windows and macOS, hide the titlebar completely on Linux
-if        /* If macOS */    (process.platform == 'darwin')  {global.TitleBarStyle = 'hiddenInset' }
-else if   /* If Windows */  (process.platform == 'win32')   {global.TitleBarStyle = 'hidden'      }
-else      /* If Linux */                                    {global.TitleBarStyle = 'default'     }
+const {app, BrowserWindow, ipcMain, ipcRenderer, Menu, MenuItem} = require('electron')
+const isDev = require('electron-is-dev');
+const {TitlebarRespect} = require('electron-titlebar-respect')
 
 // Set the appropriate icon for the operating system
 if        /* If macOS */    (process.platform == 'darwin')  {global.AppIcon = './src/base/frontend/media/images/penpot-logo/macOS/icon.icns'  }
@@ -38,6 +35,11 @@ function createWindow () {
     }
   })
   mainWindow.loadFile('src/base/index.html')
+  if (isDev) {
+    // Use default menu
+  } else {
+    Menu.setApplicationMenu(null)
+  }
   if (process.platform === 'linux') {setTimeout(() => {
     mainWindow.webContents.executeJavaScript(`document.querySelector(".linux-titlebar-buttons").style.display = 'inherit'`)
     mainWindow.webContents.executeJavaScript(`document.querySelector(".actions").style.right = '32px'`)
@@ -47,13 +49,6 @@ function createWindow () {
 
 app.whenReady().then(() => {
   createWindow()
-  // For new power saving option (Future use)
-  // powerMonitor.on('on-ac', () => { // This is only supported for Windows and macOS
-  //     console.log('The system is on AC Power (charging)');
-  // })
-  // powerMonitor.on('on-battery', () => { // This is only supported for Windows and macOS
-  //     console.log('The system is on battery');
-  // })
 })
 
 app.on('web-contents-created', function (webContentsCreatedEvent, contents) {
