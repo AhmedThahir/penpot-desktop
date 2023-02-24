ec98e8d9f643085a7bedde57af168fe7d08f9615
Korbs Studio <korbs@Korbss-iMac-Pro.local>
2023-02-23 21:48:24 -0500

    Final adjustments for macOS

--- src/process/index.js
+++ src/process/index.js
@@ -19,7 +19,7 @@ function createWindow () {
     darkTheme: true,
     // Titlebar
     titleBarStyle: global.titleBarStyle,
-    trafficLightPosition: { x: 10, y: 10 }, // for macOS
+    trafficLightPosition: { x: 10, y: 12 }, // for macOS
     titleBarOverlay: { // For Windows
       color: '#1f1f1f',
       symbolColor: 'white',
@@ -40,11 +40,16 @@ function createWindow () {
   } else {
     Menu.setApplicationMenu(null)
   }
-  if (process.platform === 'win32') {setTimeout(() => {
+  if (process.platform === 'darwin') {setTimeout(() => {
+    mainWindow.webContents.executeJavaScript(`document.querySelector("body > tab-group").shadowRoot.querySelector("div > nav").style.left = '74px'`)
+    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWF', '110px')`)
+    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWS', '350px')`)
+  }, 1500)}
+  else if (process.platform === 'win32') {setTimeout(() => {
     mainWindow.webContents.executeJavaScript(`document.querySelector(".actions").style.right = '124px'`)
     mainWindow.webContents.executeJavaScript(`document.querySelector(".actions #instance").style.right = '113px'`)
-    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWF', '160px');`)
-    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWS', '400px');`)
+    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWF', '160px')`)
+    mainWindow.webContents.executeJavaScript(`document.documentElement.style.setProperty('--navBarWS', '400px')`)
   }, 1500)}
   else if (process.platform === 'linux') {setTimeout(() => {
     mainWindow.webContents.executeJavaScript(`document.querySelector(".linux-titlebar-buttons").style.display = 'inherit'`)
