10d7fe6b2517fc6787e5042f9178ff24823ec8bc
Korbs <korbs@sudovanilla.com>
2023-05-22 19:58:35 -0400

    No longer needed

--- build/build-dmg.sh
+++
@@ -1,9 +0,0 @@
-# The package file doesn't like me using "\"
-echo "Building PKG file first..."
-echo "Please wait..."
-yarn build
-sleep 2
-clear
-echo "Building DMG file."
-echo "Please wait..."
-electron-installer-dmg ./dist/mac-arm64/Penpot\ Desktop.app Penpot-Desktop --background='./build/background-alt.png' --title='Penpot Desktop' --overwrite --icon='./build/icon.icns'
